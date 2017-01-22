407ec446cdf9eb46d97b9d92df53caac63f3ba71
mwurm93 <michaelwurm@MICHAELs-MacBook-Pro.local>
2017-01-22 11:46:01 -0500

    Initial Commit

---
+++ planit v0.2Tests/planit_v0_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  planit_v0_2Tests.swift
+//  planit v0.2Tests
+//
+//  Created by MICHAEL WURM on 1/22/17.
+//  Copyright © 2017 MICHAEL WURM. All rights reserved.
+//
+
+import XCTest
+@testable import planit_v0_2
+
+class planit_v0_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
